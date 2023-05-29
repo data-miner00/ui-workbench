7cb51d88fa6c7672c640688ece7984f8042b98c8
data-miner00 <data-miner00@users.noreply.github.com>
2023-05-29 21:12:15 +0800

    vendor: Installed tailwind to artstation

--- archives/artstation/nuxt.config.ts
+++ archives/artstation/nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title: "Artstation",
+      meta: [
+        {
+          name: "description",
+          content:
+            "A sandbox project for documenting UI components found on the internet.",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.ico",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    configPath: "./tailwind.config.js",
+    cssPath: "./assets/css/styles.css",
+    viewer: false,
+  },
+});
