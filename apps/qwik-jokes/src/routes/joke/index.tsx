3b600cf02a7292db927cfb7b278f09da52280804
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-10 21:37:11 +0800

    chore: Added qwik app

---
+++ apps/qwik-jokes/src/routes/joke/index.tsx
@@ -0,0 +1,66 @@
+import {
+  component$,
+  useSignal,
+  useStylesScoped$,
+  useTask$,
+} from "@builder.io/qwik";
+import {
+  routeLoader$,
+  routeAction$,
+  Form,
+  server$,
+} from "@builder.io/qwik-city";
+import styles from "./index.css?inline";
+
+export const useJokeVoteAction = routeAction$((props) => {
+  console.log("VOTE", props);
+});
+
+export const useDadJoke = routeLoader$(async () => {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+  return (await response.json()) as {
+    id: string;
+    status: number;
+    joke: string;
+  };
+});
+
+export default component$(() => {
+  useStylesScoped$(styles);
+  const dadJokeSignal = useDadJoke();
+  const favoriteJokeAction = useJokeVoteAction();
+  const isFavoriteSignal = useSignal(false);
+
+  useTask$(({ track }) => {
+    track(() => isFavoriteSignal.value);
+    console.log("FAVORITE (isomorphic)", isFavoriteSignal.value);
+
+    server$(() => {
+      console.log("FAVORITE (server)", isFavoriteSignal.value);
+    })();
+  });
+
+  return (
+    <section class="section bright">
+      <p>{dadJokeSignal.value.joke}</p>
+      <Form action={favoriteJokeAction}>
+        <input type="hidden" name="jokeID" value={dadJokeSignal.value.id} />
+        <button name="vote" value="up">
+          👍
+        </button>
+        <button name="vote" value="down">
+          👎
+        </button>
+      </Form>
+      <button
+        onClick$={() => {
+          isFavoriteSignal.value = !isFavoriteSignal.value;
+        }}
+      >
+        {isFavoriteSignal.value ? "❤️" : "🤍"}
+      </button>
+    </section>
+  );
+});
