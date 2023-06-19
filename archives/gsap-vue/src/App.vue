daf933677de68292af62c4965227e0269ad27a2e
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-19 18:43:38 +0800

    chore: Added gsap-vue project

---
+++ archives/gsap-vue/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { gsap, Power2 } from "gsap";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const t1 = gsap.timeline({ repeat: 2, repeatDelay: 1 });
+
+  const random = gsap.utils.random(10, 100);
+
+  t1.to("#helo", { duration: 1, x: random }, "-=0.5");
+  t1.to("#helo", { duration: 1, x: 50 }, "-=0.5");
+  t1.to("#helo", { duration: 1, opacity: 0 }, "-=0.5");
+  t1.play();
+
+  gsap.utils.shuffle([1, 2, 3, 4, 5]);
+  gsap.utils.snap(10, 23.5);
+});
+</script>
+
+<template>
+  <div id="helo">Hello from gsap</div>
+</template>
+
+<style scoped></style>
