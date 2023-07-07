0990b449aae2dd76c0fc356e245301cb9ac67b11
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-08 07:03:31 +0800

    feat: Added basic Algolia modal UI

---
+++ archives/artstation/pages/algolia.vue
@@ -0,0 +1,306 @@
+<!-- https://www.algolia.com/ -->
+<script setup lang="ts">
+type SearchHit = {
+  category: string;
+  hits: Array<string>;
+};
+
+const hits: SearchHit[] = [
+  {
+    category: "Composition API",
+    hits: ["Composition API: Dependency Injection"],
+  },
+  {
+    category: "Built-ins",
+    hits: ["Built-in Directives"],
+  },
+  {
+    category: "Reusability",
+    hits: ["Custom Directives"],
+  },
+  {
+    category: "Extra Topics",
+    hits: ["Reactivity in Depth", "Virtual DOM"],
+  },
+  {
+    category: "Best Practices",
+    hits: ["Production Deployment", "Development vs. Production"],
+  },
+  {
+    category: "Options API",
+    hits: ["data", "directives", "deactivated"],
+  },
+];
+
+const todos = ["keyboard binding", "mobile responsive", "keep states"];
+</script>
+
+<template>
+  <div
+    class="h-screen relative"
+    style="background-color: rgba(101, 108, 133, 0.8)"
+  >
+    <div
+      class="algolia absolute w-[560px] top-20 left-1/2 transform -translate-x-1/2 rounded overflow-hidden"
+    >
+      <header class="pt-4 px-4">
+        <form
+          class="flex border-green-500 h-14 border-solid border rounded px-3 items-center bg-white"
+        >
+          <label for="search_input" class="flex stroke-[1.4] text-green-500">
+            <svg
+              width="20"
+              height="20"
+              class="DocSearch-Search-Icon"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
+                stroke="currentColor"
+                fill="none"
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </label>
+          <input
+            name="search_input"
+            type="text"
+            placeholder="Search docs"
+            class="w-[80%] flex-1 h-full pl-2 outline-none text-xl"
+          />
+          <button class="stroke-[1.4]">
+            <svg width="20" height="20" viewBox="0 0 20 20">
+              <path
+                d="M10 10l5.09-5.09L10 10l5.09 5.09L10 10zm0 0L4.91 4.91 10 10l-5.09 5.09L10 10z"
+                stroke="currentColor"
+                fill="none"
+                fill-rule="evenodd"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </button>
+        </form>
+      </header>
+
+      <section class="hits max-h-[488px] overflow-y-auto px-4 pb-4">
+        <div v-for="hit in hits">
+          <div class="pt-2 px-1 leading-8 text-sm font-semibold text-green-500">
+            {{ hit.category }}
+          </div>
+          <ul>
+            <li v-for="h in hit.hits" class="mb-1 group">
+              <a
+                href=""
+                class="block p-4 bg-white text-gray-600 group-hover:bg-green-500 rounded group-hover:text-white pl-3 shadow"
+              >
+                <div class="flex pr-3 items-center">
+                  <div class="stroke-[1.4]">
+                    <svg width="20" height="20" viewBox="0 0 20 20">
+                      <path
+                        d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4"
+                        stroke="currentColor"
+                        fill="none"
+                        fill-rule="evenodd"
+                        stroke-linejoin="round"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div class="flex-1 ml-2">{{ h }}</div>
+                  <div class="stroke-[1.4] text-white">
+                    <svg class="" width="20" height="20" viewBox="0 0 20 20">
+                      <g
+                        stroke="currentColor"
+                        fill="none"
+                        fill-rule="evenodd"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M18 3v4c0 2-2 4-4 4H2"></path>
+                        <path d="M8 17l-6-6 6-6"></path>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer
+        class="flex flex-row-reverse justify-between h-11 px-3 items-center"
+      >
+        <div>
+          <a
+            href="https://www.algolia.com/ref/docsearch/?utm_source=vuejs.org&amp;utm_medium=referral&amp;utm_content=powered_by&amp;utm_campaign=docsearch"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="flex items-center"
+            ><span class="text-xs leading-6">Search by</span>
+            <svg
+              class="ml-2"
+              width="77"
+              height="19"
+              aria-label="Algolia"
+              role="img"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2196.2 500"
+            >
+              <path
+                class="cls-2"
+                d="M1070.38,275.3V5.91c0-3.63-3.24-6.39-6.82-5.83l-50.46,7.94c-2.87,.45-4.99,2.93-4.99,5.84l.17,273.22c0,12.92,0,92.7,95.97,95.49,3.33,.1,6.09-2.58,6.09-5.91v-40.78c0-2.96-2.19-5.51-5.12-5.84-34.85-4.01-34.85-47.57-34.85-54.72Z"
+              ></path>
+              <rect
+                class="cls-1"
+                x="1845.88"
+                y="104.73"
+                width="62.58"
+                height="277.9"
+                rx="5.9"
+                ry="5.9"
+              ></rect>
+              <path
+                class="cls-2"
+                d="M1851.78,71.38h50.77c3.26,0,5.9-2.64,5.9-5.9V5.9c0-3.62-3.24-6.39-6.82-5.83l-50.77,7.95c-2.87,.45-4.99,2.92-4.99,5.83v51.62c0,3.26,2.64,5.9,5.9,5.9Z"
+              ></path>
+              <path
+                class="cls-2"
+                d="M1764.03,275.3V5.91c0-3.63-3.24-6.39-6.82-5.83l-50.46,7.94c-2.87,.45-4.99,2.93-4.99,5.84l.17,273.22c0,12.92,0,92.7,95.97,95.49,3.33,.1,6.09-2.58,6.09-5.91v-40.78c0-2.96-2.19-5.51-5.12-5.84-34.85-4.01-34.85-47.57-34.85-54.72Z"
+              ></path>
+              <path
+                class="cls-2"
+                d="M1631.95,142.72c-11.14-12.25-24.83-21.65-40.78-28.31-15.92-6.53-33.26-9.85-52.07-9.85-18.78,0-36.15,3.17-51.92,9.85-15.59,6.66-29.29,16.05-40.76,28.31-11.47,12.23-20.38,26.87-26.76,44.03-6.38,17.17-9.24,37.37-9.24,58.36,0,20.99,3.19,36.87,9.55,54.21,6.38,17.32,15.14,32.11,26.45,44.36,11.29,12.23,24.83,21.62,40.6,28.46,15.77,6.83,40.12,10.33,52.4,10.48,12.25,0,36.78-3.82,52.7-10.48,15.92-6.68,29.46-16.23,40.78-28.46,11.29-12.25,20.05-27.04,26.25-44.36,6.22-17.34,9.24-33.22,9.24-54.21,0-20.99-3.34-41.19-10.03-58.36-6.38-17.17-15.14-31.8-26.43-44.03Zm-44.43,163.75c-11.47,15.75-27.56,23.7-48.09,23.7-20.55,0-36.63-7.8-48.1-23.7-11.47-15.75-17.21-34.01-17.21-61.2,0-26.89,5.59-49.14,17.06-64.87,11.45-15.75,27.54-23.52,48.07-23.52,20.55,0,36.63,7.78,48.09,23.52,11.47,15.57,17.36,37.98,17.36,64.87,0,27.19-5.72,45.3-17.19,61.2Z"
+              ></path>
+              <path
+                class="cls-2"
+                d="M894.42,104.73h-49.33c-48.36,0-90.91,25.48-115.75,64.1-14.52,22.58-22.99,49.63-22.99,78.73,0,44.89,20.13,84.92,51.59,111.1,2.93,2.6,6.05,4.98,9.31,7.14,12.86,8.49,28.11,13.47,44.52,13.47,1.23,0,2.46-.03,3.68-.09,.36-.02,.71-.05,1.07-.07,.87-.05,1.75-.11,2.62-.2,.34-.03,.68-.08,1.02-.12,.91-.1,1.82-.21,2.73-.34,.21-.03,.42-.07,.63-.1,32.89-5.07,61.56-30.82,70.9-62.81v57.83c0,3.26,2.64,5.9,5.9,5.9h50.42c3.26,0,5.9-2.64,5.9-5.9V110.63c0-3.26-2.64-5.9-5.9-5.9h-56.32Zm0,206.92c-12.2,10.16-27.97,13.98-44.84,15.12-.16,.01-.33,.03-.49,.04-1.12,.07-2.24,.1-3.36,.1-42.24,0-77.12-35.89-77.12-79.37,0-10.25,1.96-20.01,5.42-28.98,11.22-29.12,38.77-49.74,71.06-49.74h49.33v142.83Z"
+              ></path>
+              <path
+                class="cls-2"
+                d="M2133.97,104.73h-49.33c-48.36,0-90.91,25.48-115.75,64.1-14.52,22.58-22.99,49.63-22.99,78.73,0,44.89,20.13,84.92,51.59,111.1,2.93,2.6,6.05,4.98,9.31,7.14,12.86,8.49,28.11,13.47,44.52,13.47,1.23,0,2.46-.03,3.68-.09,.36-.02,.71-.05,1.07-.07,.87-.05,1.75-.11,2.62-.2,.34-.03,.68-.08,1.02-.12,.91-.1,1.82-.21,2.73-.34,.21-.03,.42-.07,.63-.1,32.89-5.07,61.56-30.82,70.9-62.81v57.83c0,3.26,2.64,5.9,5.9,5.9h50.42c3.26,0,5.9-2.64,5.9-5.9V110.63c0-3.26-2.64-5.9-5.9-5.9h-56.32Zm0,206.92c-12.2,10.16-27.97,13.98-44.84,15.12-.16,.01-.33,.03-.49,.04-1.12,.07-2.24,.1-3.36,.1-42.24,0-77.12-35.89-77.12-79.37,0-10.25,1.96-20.01,5.42-28.98,11.22-29.12,38.77-49.74,71.06-49.74h49.33v142.83Z"
+              ></path>
+              <path
+                class="cls-2"
+                d="M1314.05,104.73h-49.33c-48.36,0-90.91,25.48-115.75,64.1-11.79,18.34-19.6,39.64-22.11,62.59-.58,5.3-.88,10.68-.88,16.14s.31,11.15,.93,16.59c4.28,38.09,23.14,71.61,50.66,94.52,2.93,2.6,6.05,4.98,9.31,7.14,12.86,8.49,28.11,13.47,44.52,13.47h0c17.99,0,34.61-5.93,48.16-15.97,16.29-11.58,28.88-28.54,34.48-47.75v50.26h-.11v11.08c0,21.84-5.71,38.27-17.34,49.36-11.61,11.08-31.04,16.63-58.25,16.63-11.12,0-28.79-.59-46.6-2.41-2.83-.29-5.46,1.5-6.27,4.22l-12.78,43.11c-1.02,3.46,1.27,7.02,4.83,7.53,21.52,3.08,42.52,4.68,54.65,4.68,48.91,0,85.16-10.75,108.89-32.21,21.48-19.41,33.15-48.89,35.2-88.52V110.63c0-3.26-2.64-5.9-5.9-5.9h-56.32Zm0,64.1s.65,139.13,0,143.36c-12.08,9.77-27.11,13.59-43.49,14.7-.16,.01-.33,.03-.49,.04-1.12,.07-2.24,.1-3.36,.1-1.32,0-2.63-.03-3.94-.1-40.41-2.11-74.52-37.26-74.52-79.38,0-10.25,1.96-20.01,5.42-28.98,11.22-29.12,38.77-49.74,71.06-49.74h49.33Z"
+              ></path>
+              <path
+                class="cls-1"
+                d="M249.83,0C113.3,0,2,110.09,.03,246.16c-2,138.19,110.12,252.7,248.33,253.5,42.68,.25,83.79-10.19,120.3-30.03,3.56-1.93,4.11-6.83,1.08-9.51l-23.38-20.72c-4.75-4.21-11.51-5.4-17.36-2.92-25.48,10.84-53.17,16.38-81.71,16.03-111.68-1.37-201.91-94.29-200.13-205.96,1.76-110.26,92-199.41,202.67-199.41h202.69V407.41l-115-102.18c-3.72-3.31-9.42-2.66-12.42,1.31-18.46,24.44-48.53,39.64-81.93,37.34-46.33-3.2-83.87-40.5-87.34-86.81-4.15-55.24,39.63-101.52,94-101.52,49.18,0,89.68,37.85,93.91,85.95,.38,4.28,2.31,8.27,5.52,11.12l29.95,26.55c3.4,3.01,8.79,1.17,9.63-3.3,2.16-11.55,2.92-23.58,2.07-35.92-4.82-70.34-61.8-126.93-132.17-131.26-80.68-4.97-148.13,58.14-150.27,137.25-2.09,77.1,61.08,143.56,138.19,145.26,32.19,.71,62.03-9.41,86.14-26.95l150.26,133.2c6.44,5.71,16.61,1.14,16.61-7.47V9.48C499.66,4.25,495.42,0,490.18,0H249.83Z"
+              ></path>
+            </svg>
+          </a>
+        </div>
+        <ul class="flex items-center">
+          <li class="flex items-center">
+            <kbd>
+              <svg width="15" height="15" aria-label="Enter key" role="img">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.2"
+                >
+                  <path
+                    d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3"
+                  ></path>
+                </g>
+              </svg>
+            </kbd>
+            <span>to select</span>
+          </li>
+          <li class="flex items-center">
+            <kbd>
+              <svg width="15" height="15" aria-label="Arrow down" role="img">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.2"
+                >
+                  <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3"></path>
+                </g>
+              </svg>
+            </kbd>
+            <kbd>
+              <svg width="15" height="15" aria-label="Arrow up" role="img">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.2"
+                >
+                  <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3"></path>
+                </g>
+              </svg>
+            </kbd>
+            <span>to navigate</span>
+          </li>
+          <li class="flex items-center">
+            <kbd>
+              <svg width="15" height="15" aria-label="Escape key" role="img">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.2"
+                >
+                  <path
+                    d="M13.6167 8.936c-.1065.3583-.6883.962-1.4875.962-.7993 0-1.653-.9165-1.653-2.1258v-.5678c0-1.2548.7896-2.1016 1.653-2.1016.8634 0 1.3601.4778 1.4875 1.0724M9 6c-.1352-.4735-.7506-.9219-1.46-.8972-.7092.0246-1.344.57-1.344 1.2166s.4198.8812 1.3445.9805C8.465 7.3992 8.968 7.9337 9 8.5c.032.5663-.454 1.398-1.4595 1.398C6.6593 9.898 6 9 5.963 8.4851m-1.4748.5368c-.2635.5941-.8099.876-1.5443.876s-1.7073-.6248-1.7073-2.204v-.4603c0-1.0416.721-2.131 1.7073-2.131.9864 0 1.6425 1.031 1.5443 2.2492h-2.956"
+                  ></path>
+                </g>
+              </svg>
+            </kbd>
+            <span>to close</span>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.algolia {
+  --muted: rgba(60, 60, 60, 0.7);
+  background-color: #f9f9f9;
+  box-shadow: inset 1px 1px 0 0 hsla(0, 0%, 100%, 0.5), 0 3px 8px 0 #555a64;
+}
+
+.hits {
+  scrollbar-color: var(--muted) transparent;
+  scrollbar-width: thin;
+}
+
+footer li kbd {
+  @apply flex mr-2 pb-px rounded-sm items-center justify-center;
+  width: 20px;
+  height: 18px;
+}
+
+footer li:not(:last-of-type) {
+  margin-right: 0.8em;
+}
+
+footer {
+  background-color: white;
+  color: var(--muted);
+  box-shadow: 0 -1px 0 0 #e0e3e8, 0 -3px 6px 0 rgba(69, 98, 155, 0.12);
+}
+
+footer li span {
+  font-size: 0.75em;
+  line-height: 1.6em;
+}
+
+.cls-1,
+.cls-2 {
+  fill: #003dff;
+  fill-rule: evenodd;
+}
+</style>
