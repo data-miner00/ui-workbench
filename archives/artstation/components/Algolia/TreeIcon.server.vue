c63b292c17fb31b73f27516e7bfe6722c62848af
data-miner00 <data-miner00@users.noreply.github.com>
2023-07-08 07:23:58 +0800

    feat: Added Algolia icons

---
+++ archives/artstation/components/Algolia/TreeIcon.server.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg class="DocSearch-Hit-Tree" viewBox="0 0 24 54">
+    <g
+      stroke="currentColor"
+      fill="none"
+      fill-rule="evenodd"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M8 6v21M20 27H8.3"></path>
+    </g>
+  </svg>
+</template>
