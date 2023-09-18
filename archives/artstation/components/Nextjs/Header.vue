11ce2e23425e50069cf8c8715c4ff99c9717364d
data-miner00 <data-miner00@users.noreply.github.com>
2023-09-18 23:45:36 +0800

    feat: 1-to-1 desktop nextjs header

--- archives/artstation/components/Nextjs/Header.vue
+++ archives/artstation/components/Nextjs/Header.vue
@@ -12,7 +12,6 @@ const links = [
 <template>
   <header
     class="sticky top-0 flex flex-col justify-around items-center w-full z-20 px-6 h-16 backdrop-saturate-[180%] backdrop-blur-[5px] bg-[hsla(0,0%,100%,.8)] border-b border-solid border-[hsla(0,0%,0%,0.05)]"
-    style="box-shadow: inset 0 -1px 0 0 #eaeaea"
   >
     <nav class="max-w-[1400px] w-full relative flex-1 items-center flex">
       <div class="gap-6 w-full flex items-center">
@@ -33,23 +32,23 @@ const links = [
           >
             <path
               d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
-              fill="var(--accents-3)"
+              fill="#999"
             ></path>
           </svg>
         </a>
       </div>
       <div class="flex items-center gap-3">
         <button
-          class="w-[237px] h-8 rounded-[6px] pr-[6px] pl-2 whitespace-nowrap text-sm transition-colors duration-150 flex items-center justify-between text-[rgb(143,143,143)] bg-[rgb(242,242,242)]"
+          class="w-[251px] h-8 rounded-[6px] pr-[6px] pl-2 whitespace-nowrap text-sm transition-colors duration-150 flex items-center justify-between text-[rgb(143,143,143)] bg-[rgb(242,242,242)]"
           type="button"
         >
           Search documentation...<kbd
-            class="h-[22px] leading-[22px] rounded px-[6px] text-xs font-medium border border-solid border-[rgba(0,0,0,.08)] ml-4 bg-white text-[hsl(0,0%,40%)]"
+            class="h-[22px] leading-[22px] rounded px-[6px] text-xs font-medium border border-solid border-[rgba(0,0,0,.08)] ml-4 bg-white text-[hsl(0,0%,40%)] font-inter"
             >CtrlK</kbd
           >
         </button>
         <a
-          class="border border-solid border-[rgba(0,0,0,.08)] rounded-[6px] font-medium px-3 flex justify-center items-center transition-color duration-150 hover:bg-[#383838] h-8"
+          class="border border-solid border-[rgba(0,0,0,.08)] rounded-[6px] font-medium px-3 flex justify-center items-center transition-color duration-150 hover:bg-[hsl(0,0%,92%)] h-8 text-sm"
         >
           <span class="flex mr-[10px]"
             ><svg
@@ -71,7 +70,7 @@ const links = [
         </a>
 
         <a
-          class="flex rounded-[6px] bg-[rgb(23,23,23)] text-white text-sm h-8 items-center justify-center transition-colors duration-150 px-3 font-medium"
+          class="flex rounded-[6px] bg-[rgb(23,23,23)] text-white text-sm h-8 items-center justify-center transition-colors duration-150 px-3 font-medium hover:bg-[#383838]"
         >
           <span
             class="whitespace-nowrap overflow-hidden inline-block text-ellipsis"
