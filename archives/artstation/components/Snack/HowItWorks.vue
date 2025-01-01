427c6d7f7749c3dbcdcea580788efaa7aa7029f3
Shaun Chong <data-miner00@users.noreply.github.com>
2025-01-01 10:34:46 +0800

    feat: Add bare how it works

---
+++ archives/artstation/components/Snack/HowItWorks.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="max-w-[1200px] mx-auto my-[72px]">
+    <h2 class="text-center font-bold text-[40px] text-[#202020] mb-2">
+      How it works
+    </h2>
+    <p class="text-[#bbb] text-2xl text-center mb-8">
+      Every feature you need for seamless list creation
+    </p>
+
+    <ul class="flex gap-4 max-w-[1200px]">
+      <li class="rounded-lg bg-gray-50 p-6 flex-1 flex flex-col gap-4">
+        <div class="h-[160px] rounded-lg bg-white w-full"></div>
+        <div class="text-center">
+          <h3 class="font-semibold text-lg">Create a list</h3>
+          <p class="text-sm text-[#838383]">
+            Choose the emoji, title and description for your list
+          </p>
+        </div>
+      </li>
+
+      <li class="rounded-lg bg-gray-50 p-6 flex-1 flex flex-col gap-4">
+        <div class="h-[160px] rounded-lg bg-white w-full"></div>
+        <div class="text-center">
+          <h3 class="font-semibold text-lg">Add your links</h3>
+          <p class="text-sm text-[#838383]">
+            Type or paste any link, and rearrange them in any order
+          </p>
+        </div>
+      </li>
+      <li class="rounded-lg bg-gray-50 p-6 flex-1 flex flex-col gap-4">
+        <div class="h-[160px] rounded-lg bg-white w-full"></div>
+        <div class="text-center">
+          <h3 class="font-semibold text-lg">Share your list</h3>
+          <p class="text-sm text-[#838383]">
+            Add to your lists over time and share it anywhere online
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
--- archives/artstation/pages/snack.vue
+++ archives/artstation/pages/snack.vue
@@ -2,5 +2,6 @@
 <template>
   <div class="font-merriweather">
     <SnackMasonryHero />
+    <SnackHowItWorks />
   </div>
 </template>
